d1385ae2ee7795d2f5fe091722331ab3bd84e73a
Si Wang <siwang22@gmail.com>
2022-03-15 12:25:01 -0700

    Render the first card

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './App.css';
+import sifangwu from './data/sifangwu.json';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentCard: '0-0'
+    }
+  }
+
+  renderCard = () => {
+    const { currentCard } = this.state;
+
+    return (
+      <span>
+        {sifangwu[currentCard]?.text}
+      </span>
+    );
+  }
+
   render() {
     return (
       <div className="App">
-
+        { this.renderCard() }
       </div>
     );
   }
